8e6064469027e96dec9c3c744ada84890e8e5754
Mateus Borges <borgesm.work@gmail.com>
2022-12-14 20:20:14 -0300

    exercício 4 corrigido (acrescido de endDate IS NULL)

--- Prática - Exercícios Join/04.sql
+++ Prática - Exercícios Join/04.sql
@@ -1,5 +1,5 @@
 -- by experience id
-SELECT experiences.id, users.name AS name, roles.name AS role, companies.name AS company, experiences."startDate" FROM experiences JOIN users ON experiences."userId"=users.id JOIN roles ON experiences."roleId"=roles.id JOIN companies ON experiences."companyId"=companies.id WHERE users.id=50;
+SELECT experiences.id, users.name AS name, roles.name AS role, companies.name AS company, experiences."startDate" FROM experiences JOIN users ON experiences."userId"=users.id JOIN roles ON experiences."roleId"=roles.id JOIN companies ON experiences."companyId"=companies.id WHERE users.id=50 AND experiences."endDate" IS NULL;
 
 -- by company id
-SELECT t1.id, users.name AS name, roles.name AS role, t2.name AS company, experiences."startDate" FROM experiences JOIN companies t1 ON experiences."companyId"=t1.id JOIN users ON experiences."userId"=users.id JOIN roles ON experiences."roleId"=roles.id JOIN companies t2 ON experiences."companyId"=t2.id WHERE users.id=50;
+SELECT t1.id, users.name AS name, roles.name AS role, t2.name AS company, experiences."startDate" FROM experiences JOIN companies t1 ON experiences."companyId"=t1.id JOIN users ON experiences."userId"=users.id JOIN roles ON experiences."roleId"=roles.id JOIN companies t2 ON experiences."companyId"=t2.id WHERE users.id=50 AND experiences."endDate" IS NULL;
