f6ff4bfb65be8aba0f05f141e4e518f89471bda7
Mateus Borges <borgesm.work@gmail.com>
2022-12-14 22:00:02 -0300

    exercicios 10

---
+++ Práticas - Exercícios SQL/10.sql
@@ -0,0 +1,3 @@
+INSERT INTO compras (id_cliente,id_produto) SELECT clientes.id,produtos.id FROM clientes,produtos WHERE clientes.nome='Olga Cascais Fortunato' AND produtos.nome='Celular Topo de Linha';
+INSERT INTO compras (id_cliente,id_produto) SELECT clientes.id,produtos.id FROM clientes,produtos WHERE clientes.nome='Olga Cascais Fortunato' AND produtos.nome='Celular Topo de Linha';
+
