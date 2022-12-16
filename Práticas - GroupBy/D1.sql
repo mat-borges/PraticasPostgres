de982d09f6130f2141c3fe2d71fc3baaa92b1ace
Mateus Borges <borgesm.work@gmail.com>
2022-12-16 19:18:20 -0300

    nova query para D1 groupby

--- Práticas - GroupBy/D1.sql
+++ Práticas - GroupBy/D1.sql
@@ -1,1 +1,13 @@
 SELECT s.name AS "school", c.name AS "course", COUNT(e."userId") AS "studentsCount", e.status AS "status" FROM educations e JOIN schools s ON e."schoolId"=s.id JOIN courses c ON e."courseId"=c.id WHERE "status"='ongoing' OR "status"='finished' GROUP BY "school","course","status" ORDER BY "studentsCount" DESC LIMIT 3;
+
+-- tirando o status o exemplo fica melhor
+SELECT
+	s.name AS "school",
+	c.name AS "course",
+	COUNT(e."userId") AS "studentsCount"
+FROM educations e
+	JOIN schools s ON e."schoolId"=s.id
+	JOIN courses c ON e."courseId"=c.id
+WHERE e.status='ongoing' OR e.status='finished'
+GROUP BY s.id, c.id
+ORDER BY "studentsCount" DESC;
